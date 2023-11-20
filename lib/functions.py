b6903129c4a4d30fd37d6cd7a9df67a58963dde1
Eckra Wangui <+>
2023-11-20 15:49:17 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,23 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
+
+def greet_with_default(name= "programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    return number/2
